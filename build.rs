4311b46fc96fa60bf84e0e4c26f6ff42f5018e3a
midischwarz12 <38054771+midischwarz12@users.noreply.github.com>
2026-07-09 20:36:24 -0500

    chore: prepare protocol 0.1.17 release
    Co-authored-by: Chris Logue <17660199+omniscoder@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::path::PathBuf;
 
 fn main() {
+    if std::env::var_os("CARGO_FEATURE_GENERATED_PROTOBUF").is_none() {
+        println!("cargo:rerun-if-changed=build.rs");
+        return;
+    }
+
     let proto_dir = PathBuf::from("schemas/v1");
     let proto_file = proto_dir.join("monarchic_agent_protocol.proto");
     let mut include_dirs = vec![proto_dir.clone()];
